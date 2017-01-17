9578cd32fef050cf6ed8ec0af6f55717a8667784
tautret <autret.tristan@gmail.com>
2017-01-17 13:22:38 +0100

    Rework sprite and display BigMountain and Building

--- AdvanceWars/src/scenary/Building.java
+++ AdvanceWars/src/scenary/Building.java
@@ -1,10 +1,14 @@
 package scenary;
 
 import java.awt.Canvas;
+import java.awt.Graphics;
 
 import soldier.core.Weapon;
 
 public class Building extends Scenary{	
+	
+	private static final int MORE_PIXEL = 5;
+
 	public Building(Canvas defaultCanvas, int xx, int yy) {
 		super(defaultCanvas, xx, yy, "images/Building.png");
 	}
@@ -14,4 +18,9 @@ public class Building extends Scenary{
 		return null;
 	}
 	
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage(image.getImage(), x, y - (MORE_PIXEL * (RENDERING_SIZE/16)), RENDERING_SIZE, RENDERING_SIZE+(MORE_PIXEL * (RENDERING_SIZE/16)),
+				null);
+	}
 }
